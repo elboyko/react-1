406256423ba6eb0934db085a610866db93ea7da0
elboyko <boyko.lena@gmail.com>
2024-01-26 17:02:24 +0700

    add pie chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 
 import './App.css'
+import { Chart } from "react-google-charts";
+
+const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 5],
+  ["Eat", 2],
+  ["Learn JS", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+const options = {
+  title: "My Daily Activities",
+}
 
 function App() {
 
 
   return (
-    <>
-      <h1>Hello world</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <button>Click</button>
-    </>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"600px"}
+    />
   )
 }
 
