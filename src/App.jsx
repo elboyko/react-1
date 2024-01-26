d90d8ed3ce25fdcfda3ca0c46138f0b0fa8ea006
elboyko <boyko.lena@gmail.com>
2024-01-26 16:22:25 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <button>Click</button>
+    </>
+  )
+}
+
+export default App
